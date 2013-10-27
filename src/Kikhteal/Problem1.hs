c4684ea94ac374cbddbdb2bab35b28b1cd383798
akikhtenko <akikhtenko@gmail.com>
2013-10-27 11:20:23 +0000

    Refactored solution to problem 1

--- src/Kikhteal/Problem1.hs
+++ src/Kikhteal/Problem1.hs
@@ -1,5 +1,7 @@
 module Kikhteal.Problem1 where
 
-multiplesSum::Integer -> [Integer] -> Integer
-multiplesSum lim mul = sum $ filter (isMultiple mul) [1..lim]
+multiplesSum::Integer -> [Integer] -> Maybe Integer
+multiplesSum lim mul
+        | lim < 1 || null mul = Nothing
+        | otherwise  = Just $ sum $ filter (isMultiple mul) [1..lim]
         where isMultiple ys x = any (\y -> x `mod` y == 0) ys
--- test/Kikhteal/Problem1Spec.hs
+++ test/Kikhteal/Problem1Spec.hs
@@ -3,14 +3,22 @@ module Kikhteal.Problem1Spec (spec) where
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Kikhteal.Problem1
-import Kikhteal.QuickCheck
+import Test.QuickCheck
+import Data.Maybe
 
 spec :: Spec
 spec = do
   describe "multiplesOf3And5Sum" $ do
-    prop "never exceeds all list elements sum" $ do
-      \(SmallPositive x) (SmallPositiveList ys) -> 
-        multiplesSum x ys <= sum [1..x]
-    
+    it "results in Nothing given invalid limit" $ do
+        multiplesSum 0 [3, 5] `shouldBe` Nothing
+
+    it "results in Nothing given empty list of nums" $ do
+        multiplesSum 10 [] `shouldBe` Nothing
+
     it "results in 33 for all multiples of 3 and 5 less than 11" $ do
-        multiplesSum 10 [3, 5] `shouldBe` 33
+        multiplesSum 10 [3, 5] `shouldBe` Just 33
+
+    prop "never exceeds all list elements sum" $ do
+      forAll (choose (1,1000)) $ \x ->
+        forAll (sized $ \s -> choose (1, s `min` 10) >>= \k -> vectorOf k (choose (1,1000))) $ \ys -> 
+          not (null ys) ==> fromJust (multiplesSum x ys) <= sum [1..x]
